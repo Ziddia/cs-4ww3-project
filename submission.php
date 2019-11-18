a8421c938c8f0a856dcc96dea3efc8a60b48a14d
Joshua Smith <smithj12@mcmaster.ca>
2019-11-18 15:47:15 -0500

    implement submission form, it will only submit by logged in users + will upload images/videos to aws

--- submission.php
+++ submission.php
@@ -23,20 +23,20 @@
 	<!-- Main page content -->
 	<section>
 		<h3>Add a New Transit System</h3>
-		<form action="#" method="POST" class="header-buffer">
+		<form action="submission_post.php" method="POST" class="header-buffer" enctype="multipart/form-data">
 			<div class="form-group row">
 				<label for="obj_name" class="col-md-2 col-form-label">Name</label>
-				<input type="text" class="form-control col-md-10" id="obj_name" required>
+				<input type="text" class="form-control col-md-10" name="obj_name" id="obj_name" required>
 			</div>
 			<div class="form-group row">
 				<label for="obj_desc" class="col-md-2 col-form-label">Description</label>
-				<input type="text" class="form-control col-md-10" id="obj_desc" required>
+				<input type="text" class="form-control col-md-10" name="obj_desc" id="obj_desc" required>
 			</div>
 			<!-- Use a select to provide transit options, these could be pulled dynamically in future.
 			Actually what I want is to have some sort of autocomplete location search from google maps API. -->
 			<div class="form-group row">
 				<label for="obj_type" class="col-md-2 col-form-label">Type</label>
-				<select class="form-control col-md-10" id="obj_type">
+				<select class="form-control col-md-10" name="obj_type" id="obj_type">
 					<option>Train</option>
 					<option>Bus</option>
 					<option>Tram</option>
@@ -46,21 +46,21 @@
 			</div>
 			<div class="form-group row">
 				<label for="obj_lat" class="col-md-2 col-form-label">Latitude</label>
-				<input type="number" step="any" class="form-control col-md-7" id="obj_lat" required>
+				<input type="number" step="any" class="form-control col-md-7" name="obj_lat" id="obj_lat" required>
 			</div>
 			<div class="form-group row">
 				<label for="obj_long" class="col-md-2 col-form-label">Longitude</label>
-				<input type="number" step="any" class="form-control col-md-7" id="obj_long" required>
+				<input type="number" step="any" class="form-control col-md-7" name="obj_long" id="obj_long" required>
 			</div>
 			<div class="form-group row">
 				<label for="obj_city" class="col-md-2 col-form-label">City</label>
-				<input type="text" class="form-control col-md-7" id="obj_city">
+				<input type="text" class="form-control col-md-7" name="obj_city" id="obj_city">
 				<button type="button" data-toggle="modal" data-target="#modal_location" class="btn btn-secondary col-md-2 offset-md-1" onclick="navigator.geolocation.getCurrentPosition(LoadLocation);">Find Near Me</button>
 			</div>
 			<!-- These could also be pulled dynamically -->
 			<div class="form-group row">
 				<label for="obj_province" class="col-md-2 col-form-label">Province</label>
-				<select class="form-control col-md-10" id="obj_province">
+				<select class="form-control col-md-10" name="obj_province" id="obj_province">
 					<option>AB</option>
 					<option>BC</option>
 					<option>MB</option>
@@ -78,18 +78,18 @@
 			</div>
 			<!-- URL of external information about the transit line -->
 			<div class="form-group row">
-				<label for="obj_url" class="col-md-2 col-form-label" pattern="https?://.+">External URL</label>
-				<input type="url" class="form-control col-md-10" id="obj_url">
+				<label for="obj_url" class="col-md-2 col-form-label">External URL</label>
+				<input type="url" class="form-control col-md-10" name="obj_url" id="obj_url" pattern="https?://.+">
 			</div>
 			<!-- Image upload -->
 			<div class="form-group row">
 				<label for="obj_img" class="col-md-2 col-form-label">Upload Image</label>
-				<input type="file" class="form-control-file col-md-10" id="obj_img" accept=".jpg, .png">
+				<input type="file" class="form-control-file col-md-10" name="obj_img" id="obj_img" accept=".jpg, .png">
 			</div>
 			<!-- Video upload -->
 			<div class="form-group row">
 				<label for="obj_vid" class="col-md-2 col-form-label">Upload Video</label>
-				<input type="file" class="form-control-file col-md-10" id="obj_vid" accept=".mp4, .webm">
+				<input type="file" class="form-control-file col-md-10" name="obj_vid" id="obj_vid" accept=".mp4, .webm">
 			</div>
 			<button type="submit" class="btn btn-secondary">Submit</button>
 		</form>
---
+++ submission_post.php
@@ -0,0 +1,187 @@
+<?php
+
+require 'includes/aws.phar';
+
+// aws setup stuff
+use Aws\S3\S3Client;
+
+use Aws\Exception\AwsException;
+
+// start session if its not already started
+if(!isset($_SESSION)) session_start();
+
+// array to store all validation errors in
+$errs = [];
+
+// confirm the user is logged in
+if (!isset($_SESSION["username"])) {
+	$errs[] = "You must be logged in to submit a station.";
+}
+
+// check that all required parameters were sent as POST
+if (empty($_POST["obj_name"]) || empty($_POST["obj_type"]) || empty($_POST["obj_lat"]) || empty($_POST["obj_long"])) {
+	$errs[] = "Form data is incomplete, missing at least one of: station name, type, latitude, or longitude.";
+}
+
+// confirm that type is valid
+$valid_types = array("Train", "Bus", "Tram", "Subway", "Ferry");
+if (!in_array($_POST["obj_type"], $valid_types)) {
+	$errs[] = "Invalid type provided for this station.";
+}
+
+// we have some size constraints in the db, validate entry meets these constraints
+if (!empty($_POST["obj_name"]) && strlen($_POST["obj_name"]) > 60) {
+	$errs[] = "Station name is too long, please ensure it is less than 60 characters long.";
+}
+
+if (!empty($_POST["obj_desc"]) && strlen($_POST["obj_desc"]) > 100) {
+	$errs[] = "Station description is too long, please ensure it is less than 100 characters long.";
+}
+
+if (!empty($_POST["obj_city"]) && strlen($_POST["obj_city"]) > 30) {
+	$errs[] = "Station city is too long, please ensure it is less than 30 characters long.";
+}
+
+if (!empty($_POST["obj_url"]) && strlen($_POST["obj_url"]) > 100) {
+	$errs[] = "Station name is too long, please ensure it is less than 60 characters long.";
+}
+
+// TODO: validate latitude and longitude. do any necessary validations on image and video.
+
+// only proceed if no validation errors were caught
+if (count($errs) === 0) {
+	// get the variables for database connection
+	include('includes/db.php');
+
+	try {
+		// establish connection
+		$pdo = new PDO($dsn, $user, $pass, $options);
+
+		// load the user's account ID to be inserted as foreign key
+		$id_query = $pdo->prepare("SELECT id FROM users WHERE username = ?");
+		$id_query->execute([$_SESSION["username"]]);
+		$user_id = $id_query->fetch(PDO::FETCH_OBJ);
+
+		if (!isset($user_id->id)) {
+			$errs[] = "Something went wrong while retrieving your profile from the database.";
+		} else {
+			// TODO: validate that this object doesn't already exist.
+			// realistically though this is impossible, outside of checking latitude/longitude.
+			// and this is still not a good method since minor changes to lat/long will still be
+			// roughly the same location, but will slip through the validation.
+
+			// The same options that can be provided to a specific client constructor can also be supplied to the Aws\Sdk class.
+			// I set up the bucket in ca-central-1
+			$sharedConfig = [
+			    'region' => 'ca-central-1',
+			    'version' => 'latest'
+			];
+
+			// Create an SDK class used to share configuration across clients.
+			$sdk = new Aws\Sdk($sharedConfig);
+
+			// Create an Amazon S3 client using the shared configuration data.
+			$s3Client = $sdk->createS3();
+
+			// create a unique key to store the object into s3 with
+			$key = uniqid($_SESSION["username"], true);
+
+			// Send a PutObject request and get the result object.
+			$result = $s3Client->putObject([
+			    'Bucket' => 'transitrating.tk-uploads',
+			    'Key' => $key,
+			    'Body' => file_get_contents($_FILES['obj_img']['tmp_name'])
+			]);
+
+			// do it again for the video
+			$key_vid = uniqid($_SESSION["username"], true);
+
+			// Send a PutObject request and get the result object.
+			$result = $s3Client->putObject([
+			    'Bucket' => 'transitrating.tk-uploads',
+			    'Key' => $key_vid,
+			    'Body' => file_get_contents($_FILES['obj_vid']['tmp_name'])
+			]);
+
+			// collect variables to use in the query
+			$name = $_POST["obj_name"];
+			$type = $_POST["obj_type"];
+			$lat = $_POST["obj_lat"];
+			$long = $_POST["obj_long"];
+
+			$desc = "";
+			if (isset($_POST["obj_desc"])) $desc = $_POST["obj_desc"];
+
+			$city = "";
+			if (isset($_POST["obj_city"])) $city = $_POST["obj_city"];
+
+			$province = "";
+			if (isset($_POST["obj_province"])) $province = $_POST["obj_province"];
+
+			$url = "";
+			if (isset($_POST["obj_url"])) $url = $_POST["obj_url"];
+
+			// insert into the database
+			$pdo->prepare("INSERT INTO stations (`name`, `desc`, `type`, latitude, longitude, city, province, url, image, video, uploader) VALUES (?,?,?,?,?,?,?,?,?,?,?)")->execute([$name,$desc,$type,$lat,$long,$city,$province,$url,$key,$key_vid,$user_id->id]);
+		}
+	} catch (\PDOException $e) {
+		$errs[] = "An error occurred while communicating with the database.";
+		throw new \PDOException($e->getMessage(), (int)$e->getCode());
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Transit Rating</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="static/index.css">
+	<link rel="stylesheet" type="text/css" href="static/base.css">
+	<link rel="stylesheet" type="text/css" href="static/bootstrap/css/bootstrap.min.css">
+	<!-- need this sheet for hamburger icon for mobile nav -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+	<?php include("includes/header.php"); ?>
+	<?php 
+		$page_id = 'submit';
+		include("includes/navbar.php"); 
+	?>
+
+	<?php include("includes/login-modal.php"); ?>
+
+	<!-- Main page content -->
+	<section>
+		<?php
+			// if any errors occurred, list them for the user.
+			// otherwise, report that registration was successful.
+			if (count($errs) > 0) {
+				echo "<p>Submission was unsuccessful. Please review the errors below.</p>";
+				foreach ($errs as $err) {
+					echo "<p>".$err."</p>";
+				}
+				echo "<p>Click <a href=\"submission.php\">here</a> to retry submission.</p>";
+			} else {
+				echo "<p>Submission completed successfully.</p>";
+			}
+		?>
+	</section>
+
+	<footer>
+		<h5>Transit Rating</h5>
+		<h6>Helping You Get Places</h6>
+	</footer>
+
+	<!-- jQuery/Bootstrap scripts -->
+	<script
+	  src="https://code.jquery.com/jquery-3.4.1.min.js"
+	  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+	  crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
